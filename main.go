24d767a1e3b35c9fc93d164abdd7cab1a4d32873
artemisaix <artemisaix@gmail.com>
2024-02-08 11:51:42 -0600

    log y registro de log en un file

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 )
 
 func main() {
+	log.SetPrefix("main():")
 	str := "123"
 	num, err := strconv.Atoi(str)
 
@@ -16,7 +18,7 @@ func main() {
 	}
 
 	divide(10, 2)
-	divide(10, 0)
+	// divide(10, 0)
 	divide(10, 3)
 
 	fmt.Println(num)
@@ -35,7 +37,21 @@ func main() {
 	}
 	defer file.Close()
 
-	//uso del panic
+	file, err = os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.SetOutput(file) // lo escribe en un archivo de log
+	log.Print("Oye,soy un log en el file")
+	defer file.Close() //se cierra el fujo del archivo de log
+
+	//uso de logs()
+
+	//log.Panic("No se puede continuar") //detiene la ejecucion del programa
+	log.Print("Mensaje de registros...")
+	//log.Fatal("No se puede continuar") //detiene la ejecucion del programa
+	log.Println("Este es otro mensaje de registros...")
 
 }
 
